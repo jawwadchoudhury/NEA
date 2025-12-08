617890628b4805d46c5d0e1cdcdc34c643096e79
Jawwad Choudhury <244680@esher.ac.uk>
2025-12-08 23:36:11 +0000

    Add project files.

---
+++ maze_nea/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace maze_nea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mazePanel = new System.Windows.Forms.Panel();
+            this.widthUpDown = new System.Windows.Forms.NumericUpDown();
+            this.heightUpDown = new System.Windows.Forms.NumericUpDown();
+            this.widthLabel = new System.Windows.Forms.Label();
+            this.heightLabel = new System.Windows.Forms.Label();
+            this.generateMazeButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.widthUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mazePanel
+            // 
+            this.mazePanel.Location = new System.Drawing.Point(75, 30);
+            this.mazePanel.Name = "mazePanel";
+            this.mazePanel.Size = new System.Drawing.Size(550, 550);
+            this.mazePanel.TabIndex = 0;
+            // 
+            // widthUpDown
+            // 
+            this.widthUpDown.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.widthUpDown.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.widthUpDown.Location = new System.Drawing.Point(477, 616);
+            this.widthUpDown.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.widthUpDown.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.widthUpDown.Name = "widthUpDown";
+            this.widthUpDown.Size = new System.Drawing.Size(65, 23);
+            this.widthUpDown.TabIndex = 1;
+            this.widthUpDown.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.widthUpDown.ValueChanged += new System.EventHandler(this.widthUpDown_ValueChanged);
+            // 
+            // heightUpDown
+            // 
+            this.heightUpDown.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.heightUpDown.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.heightUpDown.Location = new System.Drawing.Point(560, 616);
+            this.heightUpDown.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.heightUpDown.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.heightUpDown.Name = "heightUpDown";
+            this.heightUpDown.Size = new System.Drawing.Size(65, 23);
+            this.heightUpDown.TabIndex = 2;
+            this.heightUpDown.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.heightUpDown.ValueChanged += new System.EventHandler(this.heightUpDown_ValueChanged);
+            // 
+            // widthLabel
+            // 
+            this.widthLabel.AutoSize = true;
+            this.widthLabel.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.widthLabel.Location = new System.Drawing.Point(474, 598);
+            this.widthLabel.Name = "widthLabel";
+            this.widthLabel.Size = new System.Drawing.Size(39, 15);
+            this.widthLabel.TabIndex = 3;
+            this.widthLabel.Text = "Width";
+            // 
+            // heightLabel
+            // 
+            this.heightLabel.AutoSize = true;
+            this.heightLabel.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.heightLabel.Location = new System.Drawing.Point(557, 598);
+            this.heightLabel.Name = "heightLabel";
+            this.heightLabel.Size = new System.Drawing.Size(43, 15);
+            this.heightLabel.TabIndex = 4;
+            this.heightLabel.Text = "Height";
+            // 
+            // generateMazeButton
+            // 
+            this.generateMazeButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.generateMazeButton.Location = new System.Drawing.Point(75, 598);
+            this.generateMazeButton.Name = "generateMazeButton";
+            this.generateMazeButton.Size = new System.Drawing.Size(150, 45);
+            this.generateMazeButton.TabIndex = 5;
+            this.generateMazeButton.Text = "Generate Maze";
+            this.generateMazeButton.UseVisualStyleBackColor = true;
+            this.generateMazeButton.Click += new System.EventHandler(this.generateMazeButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(1, 0);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(63, 13);
+            this.outputLabel.TabIndex = 6;
+            this.outputLabel.Text = "outputLabel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 661);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.generateMazeButton);
+            this.Controls.Add(this.heightLabel);
+            this.Controls.Add(this.widthLabel);
+            this.Controls.Add(this.heightUpDown);
+            this.Controls.Add(this.widthUpDown);
+            this.Controls.Add(this.mazePanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Maze NEA";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.widthUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mazePanel;
+        private System.Windows.Forms.NumericUpDown widthUpDown;
+        private System.Windows.Forms.NumericUpDown heightUpDown;
+        private System.Windows.Forms.Label widthLabel;
+        private System.Windows.Forms.Label heightLabel;
+        private System.Windows.Forms.Button generateMazeButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ maze_nea/Form1.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace maze_nea
+{
+    public partial class Form1 : Form
+    {
+        Maze maze = new Maze(3, 3);
+        Pen pen = new Pen(Color.Black, 5);
+        Boolean isGenerating = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Maze
+        {
+            public List<Node> Nodes { get; private set; } = new List<Node>();
+            public int Width { get; private set; }
+            public int Height { get; private set; }
+            public int NodeSize { get; private set; }
+            public int EndNodeIndex { get; private set; }
+            public Maze(int width, int height)
+            {
+                Width = width;
+                Height = height;
+            }
+            public void addNode(Node node)
+            {
+                Nodes.Add(node);
+            }
+            public void clearNodes()
+            {
+                Nodes.Clear();
+            }
+            public void setWidth(int width)
+            {
+                Width = width;
+            }
+            public void setHeight(int height)
+            {
+                Height = height;
+            }
+            public void setNodeSize(int nodeSize)
+            {
+                NodeSize = nodeSize;
+            }
+            public void setEndNodeIndex(int endNodeIndex)
+            {
+                EndNodeIndex = endNodeIndex;
+            }
+        }
+        public class Node
+        {
+            public int DecimalValue { get; private set; }
+            public int X { get; private set; }
+            public int Y { get; private set; }
+            public int Index { get; private set; }
+
+            public Node(int decimalValue)
+            {
+                DecimalValue = decimalValue;
+            }
+            public bool getTopValue()
+            {
+                return ((uint)DecimalValue & 1) == 1;
+            }
+            public bool getRightValue()
+            {
+                return ((uint)DecimalValue >> 1 & 1) == 1;
+            }
+            public bool getBottomValue()
+            {
+                return ((uint)DecimalValue >> 2 & 1) == 1;
+            }
+            public bool getLeftValue()
+            {
+                return ((uint)DecimalValue >> 3 & 1) == 1;
+            }
+            public void toggleTopValue()
+            {
+                DecimalValue ^= 1;
+            }
+            public void toggleRightValue()
+            {
+                DecimalValue ^= 2;
+            }
+            public void toggleBottomValue()
+            {
+                DecimalValue ^= 4;
+            }
+            public void toggleLeftValue()
+            {
+                DecimalValue ^= 8;
+            }
+            public void removeTopValue()
+            {
+                DecimalValue &= ~1;
+            }
+            public void removeRightValue()
+            {
+                DecimalValue &= ~2;
+            }
+            public void removeBottomValue()
+            {
+                DecimalValue &= ~4;
+            }
+            public void removeLeftValue()
+            {
+                DecimalValue &= ~8;
+            }
+            public void setDecimalValue(int decimalValue)
+            {
+                DecimalValue = decimalValue;
+            }
+            private int getWallCount()
+            {
+                int wallCount = 0;
+                if (getTopValue()) wallCount++;
+                if (getRightValue()) wallCount++;
+                if (getBottomValue()) wallCount++;
+                if (getLeftValue()) wallCount++;
+                return wallCount;
+            }
+            public void setX(int x)
+            {
+                X = x;
+            }
+            public void setY(int y)
+            {
+                Y = y;
+            }
+            public void setIndex(int index)
+            {
+                Index = index;
+            }
+            public void Draw(Graphics g, int NodeSize, Pen Pen)
+            {
+                if (((uint)DecimalValue & 1) == 1)
+                {
+                    g.DrawLine(Pen, new Point(0, 0), new Point(NodeSize, 0));
+                }
+                if (((uint)DecimalValue >> 1 & 1) == 1)
+                {
+                    g.DrawLine(Pen, new Point(NodeSize, 0), new Point(NodeSize, NodeSize));
+                }
+                if (((uint)DecimalValue >> 2 & 1) == 1)
+                {
+                    g.DrawLine(Pen, new Point(0, NodeSize), new Point(NodeSize, NodeSize));
+                }
+                if (((uint)DecimalValue >> 3 & 1) == 1)
+                {
+                    g.DrawLine(Pen, new Point(0, 0), new Point(0, NodeSize));
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            generateEmptyMaze(maze.Width, maze.Height);
+        }
+
+        private void generateEmptyMaze(int width, int height)
+        {
+            maze.clearNodes();
+            maze.setNodeSize(Math.Min(540 / width, 540 / height));
+            for (int i = 0; i < width * height; i++)
+            {
+                Node node = new Node(15);
+                node.setX(i % width);
+                node.setY(i / width);
+                node.setIndex(i);
+                maze.addNode(node);
+            }
+            drawMaze();
+        }
+        private async void primsAlgorithm()
+        {
+            int startIndex = new Random().Next(0, maze.Nodes.Count);
+            List<int> primaryIndexes = new List<int>();
+            List<int> frontierIndexes = new List<int>();
+            primaryIndexes.Add(startIndex); // Turn the starting node into a primary node
+
+            void getFrontierIndexes(int index)
+            {
+                int x = index % maze.Width; // Column
+                int y = index / maze.Width; // Row
+
+                if (x != maze.Width - 1 && !primaryIndexes.Contains(index + 1) && !frontierIndexes.Contains(index + 1))
+                {
+                    frontierIndexes.Add(index + 1);
+                }
+                if (x != 0 && !primaryIndexes.Contains(index - 1) && !frontierIndexes.Contains(index - 1))
+                {
+                    frontierIndexes.Add(index - 1);
+                }
+                if (y != maze.Height - 1 && !primaryIndexes.Contains(index + maze.Width) && !frontierIndexes.Contains(index + maze.Width))
+                {
+                    frontierIndexes.Add(index + maze.Width);
+                }
+                if (y != 0 && !primaryIndexes.Contains(index - maze.Width) && !frontierIndexes.Contains(index - maze.Width))
+                {
+                    frontierIndexes.Add(index - maze.Width);
+                }
+            }
+            void refreshColours()
+            {
+                foreach (Control control in mazePanel.Controls)
+                {
+                    control.BackColor = Color.Gray;
+                }
+                foreach (int index in primaryIndexes)
+                {
+                    mazePanel.Controls[index].BackColor = Color.LightGoldenrodYellow;
+                }
+                foreach (int index in frontierIndexes)
+                {
+                    mazePanel.Controls[index].BackColor = Color.LightGreen;
+                }
+            }
+
+            getFrontierIndexes(startIndex);
+            refreshColours();
+
+            Random random = new Random();
+            while (frontierIndexes.Count > 0)
+            {
+                await Task.Delay(20);
+                int frontierIndex = frontierIndexes[new Random().Next(0, frontierIndexes.Count)];
+                List<int> currentFrontiers = new List<int>();
+                int x = frontierIndex % maze.Width; // Column
+                int y = frontierIndex / maze.Width; // Row
+                if (x != maze.Width - 1 && primaryIndexes.Contains(frontierIndex + 1))
+                {
+                    currentFrontiers.Add(frontierIndex + 1);
+                }
+                if (x != 0 && primaryIndexes.Contains(frontierIndex - 1))
+                {
+                    currentFrontiers.Add(frontierIndex - 1);
+                }
+                if (y != maze.Height - 1 && primaryIndexes.Contains(frontierIndex + maze.Width))
+                {
+                    currentFrontiers.Add(frontierIndex + maze.Width);
+                }
+                if (y != 0 && primaryIndexes.Contains(frontierIndex - maze.Width))
+                {
+                    currentFrontiers.Add(frontierIndex - maze.Width);
+                }
+
+                if (currentFrontiers.Count > 0)
+                {
+                    int primaryIndex = currentFrontiers[random.Next(0, currentFrontiers.Count)];
+                    // Create tunnel between randomFrontierIndex and randomAdjacentPrimaryIndex
+                    // (Implementation of tunnel creation goes here)
+                    int getDirection(int indexA, int indexB)
+                    {
+                        if (indexA - maze.Width == indexB) return 0; // Up
+                        if (indexA + 1 == indexB) return 1; // Right
+                        if (indexA + maze.Width == indexB) return 2; // Down
+                        if (indexA - 1 == indexB) return 3; // Left
+                        return -1;
+                    }
+                    switch (getDirection(primaryIndex, frontierIndex))
+                    {
+                        case 0:
+                            maze.Nodes[primaryIndex].removeTopValue();
+                            maze.Nodes[frontierIndex].removeBottomValue();
+                            break;
+                        case 1:
+                            maze.Nodes[primaryIndex].removeRightValue();
+                            maze.Nodes[frontierIndex].removeLeftValue();
+                            break;
+                        case 2:
+                            maze.Nodes[primaryIndex].removeBottomValue();
+                            maze.Nodes[frontierIndex].removeTopValue();
+                            break;
+                        case 3:
+                            maze.Nodes[primaryIndex].removeLeftValue();
+                            maze.Nodes[frontierIndex].removeRightValue();
+                            break;
+                        case -1:
+                            MessageBox.Show("Invalid direction");
+                            break;
+                    }
+                    primaryIndexes.Add(frontierIndex);
+                    frontierIndexes.Remove(frontierIndex);
+                    getFrontierIndexes(frontierIndex);
+                    refreshColours();
+                }
+            }
+
+            //Block to create an exit (on one of the edge nodes)
+            switch (random.Next(0, 4))
+            {
+                case 0:
+                    int exitIndex = random.Next(0, maze.Width);
+                    maze.setEndNodeIndex(exitIndex);
+                    maze.Nodes[exitIndex].removeTopValue();
+                    break;
+                case 1:
+                    exitIndex = random.Next(0, maze.Height) * maze.Width + (maze.Width - 1);
+                    maze.setEndNodeIndex(exitIndex);
+                    maze.Nodes[exitIndex].removeRightValue();
+                    break;
+                case 2:
+                    exitIndex = maze.Width * (maze.Height - 1) + random.Next(0, maze.Width);
+                    maze.setEndNodeIndex(exitIndex);
+                    maze.Nodes[exitIndex].removeBottomValue();
+                    break;
+                case 3:
+                    exitIndex = maze.Width * random.Next(0, maze.Height);
+                    maze.setEndNodeIndex(exitIndex);
+                    maze.Nodes[exitIndex].removeLeftValue();
+                    break;
+            }
+            foreach (Control control in mazePanel.Controls)
+            {
+                control.BackColor = DefaultBackColor;
+            }
+            isGenerating = false;
+        }
+
+        private void drawMaze()
+        {
+            mazePanel.Controls.Clear();
+            int nodeSize = maze.NodeSize;
+            int mazeWidth = maze.Width * nodeSize;
+            int mazeHeight = maze.Height * nodeSize;
+            mazePanel.Size = new Size(mazeWidth, mazeHeight);
+            for (int i = 0; i < maze.Nodes.Count; i++)
+            {
+                Node node = maze.Nodes[i];
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.Location = new Point(node.X * nodeSize, node.Y * nodeSize);
+                pictureBox.Size = new Size(nodeSize, nodeSize);
+                pictureBox.Tag = node;
+                pictureBox.BackColor = Color.Gray;
+                pictureBox.Paint += pictureBox_Paint;
+                mazePanel.Controls.Add(pictureBox);
+            }
+        }
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            PictureBox pictureBox = (PictureBox)sender;
+            Node node = (Node)pictureBox.Tag;
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            node.Draw(e.Graphics, maze.NodeSize, pen);
+        }
+        private void generateMazeButton_Click(object sender, EventArgs e)
+        {
+            if (!isGenerating) {
+                isGenerating = true;
+                generateEmptyMaze(maze.Width, maze.Height);
+                primsAlgorithm();
+            }
+        }
+
+        private void widthUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            maze.setWidth((int)widthUpDown.Value);
+            generateEmptyMaze(maze.Width, maze.Height);
+        }
+
+        private void heightUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            maze.setHeight((int)heightUpDown.Value);
+            generateEmptyMaze(maze.Width, maze.Height);
+        }
+    }
+}
